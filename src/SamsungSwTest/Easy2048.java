ca898e87b958c3a8b7b8755024465b9fa89e7d2e
정원균 <wjddnjsrbs97@naver.com>
2023-02-07 23:38:40 +0900

    2023.02.07

--- src/SamsungSwTest/Easy2048.java
+++ src/SamsungSwTest/Easy2048.java
@@ -47,12 +47,12 @@ public class Easy2048 {
 	}
 	
 	public static void start(int depth, int[][]play) {
-		if(depth == 5) {
+		if(depth == 3) {
 			
 //			for(int i = 0; i < N; i++) {
 //				for(int j = 0; j < N; j++) {
 //					System.out.printf("%d ",play[i][j]);
-//				}	
+//			}	
 //			System.out.println();
 //			}
 //			System.out.println();
@@ -70,9 +70,20 @@ public class Easy2048 {
 		}
 		
 		for(int dir : mode) {
+			System.out.println("Original");
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					System.out.printf("%d ",play[i][j]);
+				}
+				System.out.println();
+			}	
+			System.out.println("--------------------");
+			
+			
 			switch(dir) {
 				//east
 				case 1:
+					System.out.println("right");
 					for(int i = 0; i < N; i++) {
 						//일단 그 행에 대한 배열 초기화 -> 한 쪽으로 밀기 위해서 
 						for(int j = N-1; j>=0 ; j--) {
@@ -85,14 +96,13 @@ public class Easy2048 {
 							}
 						}
 						//동쪽 끝에서부터 하나씩 추가 if 같은게 있다면 더해준다 & 한 번 합치고 나면 flag = false로 대입 
-						boolean flag = true;
 						int idx = N-1;
 						int temp = -1;
 						while(!dq.isEmpty()) {
 							//System.out.println(dq);
 							int now = dq.removeFirst();
 							//만약 합쳐질 수 있고 이전 값과 지금 값이 같을 경우 합쳐준다 
-							if(flag==true && temp == now) {
+							if(temp == now) {
 								//play[i][idx+1] *= 2;
 								copy[i][idx+1] *= 2;
 								temp = -1;
@@ -102,7 +112,6 @@ public class Easy2048 {
 								//play[i][idx] = now;
 								copy[i][idx] = now;
 								temp = now;
-								flag = true;
 								idx--;
 							}
 						}
@@ -111,6 +120,7 @@ public class Easy2048 {
 				
 				//west	
 				case 2:
+					System.out.println("left");
 					for(int i = 0; i < N; i++) {
 						//일단 그 행에 대한 배열 초기화 -> 한 쪽으로 밀기 위해서 
 						for(int j = 0; j< N ; j++) {
@@ -123,24 +133,22 @@ public class Easy2048 {
 							}
 						}
 						//동쪽 끝에서부터 하나씩 추가 if 같은게 있다면 더해준다 & 한 번 합치고 나면 flag = false로 대입 
-						boolean flag = true;
 						int idx = 0;
 						int temp = -1;
 						while(!dq.isEmpty()) {
 							//System.out.println(dq);
 							int now = dq.removeFirst();
 							//만약 합쳐질 수 있고 이전 값과 지금 값이 같을 경우 합쳐준다 
-							if(flag==true && temp == now) {
+							if(temp == now) {
 								//play[i][idx-1] *= 2; 
 								copy[i][idx-1] *= 2; 
-								flag = false;
+								temp = -1;
 							}
 							//합쳐질 수 없는 case (같거나 다르거나 상관 x )
 							else {
 								//play[i][idx] = now;
 								copy[i][idx] = now;
 								temp = now;
-								flag = true;
 								idx++;
 							}
 						}
@@ -150,6 +158,7 @@ public class Easy2048 {
 				
 				//south
 				case 3:
+					System.out.println("down");
 					for(int i = 0; i < N; i++) {
 						//일단 그 행에 대한 배열 초기화 -> 한 쪽으로 밀기 위해서 
 						for(int j = N-1; j>=0 ; j--) {
@@ -159,29 +168,45 @@ public class Easy2048 {
 								//원래 있던 값은 초기화 
 								//play[j][i] = 0;
 								copy[j][i] = 0;
+								System.out.println("j: "+j+" i: "+i+" "+copy[j][i]);
 							}
 						}
 						//동쪽 끝에서부터 하나씩 추가 if 같은게 있다면 더해준다 & 한 번 합치고 나면 flag = false로 대입 
-						boolean flag = true;
 						int idx = N-1;
 						int temp = -1;
 						while(!dq.isEmpty()) {
-							//System.out.println(dq);
+							for(int a = 0; a < N; a++) {
+								for(int b = 0; b < N; b++) {
+									System.out.printf("%d ",copy[a][b]);
+								}
+								System.out.println();
+							}
+							System.out.println(dq);
 							int now = dq.removeFirst();
 							//만약 합쳐질 수 있고 이전 값과 지금 값이 같을 경우 합쳐준다 
-							if(flag==true && temp == now) {
+							if(temp == now) {
 								//play[idx+1][i] *= 2; 
+								System.out.println("i: "+i+" idx: "+idx);
 								copy[idx+1][i] *= 2; 
-								flag = false;
+								System.out.println("change "+copy[idx+1][i]/2+" to "+copy[idx+1][i]);
+								temp = -1;
 							}
 							//합쳐질 수 없는 case (같거나 다르거나 상관 x )
 							else {
 								//play[idx][i] = now;
+								System.out.println("i: "+i+" idx: "+idx);
+								
 								copy[idx][i] = now;
+								System.out.println("input: " + copy[idx][i]);
 								temp = now;
-								flag = true;
 								idx--;
 							}
+							for(int a = 0; a < N; a++) {
+								for(int b = 0; b < N; b++) {
+									System.out.printf("%d ",copy[a][b]);
+								}
+								System.out.println();
+							}	
 						}
 					}
 					
@@ -189,9 +214,10 @@ public class Easy2048 {
 				
 				//north
 				case 4:
+					System.out.println("up");
 					for(int i = 0; i < N; i++) {
 						//일단 그 행에 대한 배열 초기화 -> 한 쪽으로 밀기 위해서 
-						for(int j = 0; j< N ; j++) {
+						for(int j = 0; j < N ; j++) {
 							if(play[j][i]!=0) {
 								//일단 queue에 넣고 
 								dq.add(play[j][i]);
@@ -208,23 +234,34 @@ public class Easy2048 {
 							//System.out.println(dq);
 							int now = dq.removeFirst();
 							//만약 합쳐질 수 있고 이전 값과 지금 값이 같을 경우 합쳐준다 
-							if(flag==true && temp == now) {
+							if(temp == now) {
 								//play[idx-1][i] *= 2;
 								copy[idx-1][i] *= 2; 
-								flag = false;
+								temp = -1;
 							}
 							//합쳐질 수 없는 case (같거나 다르거나 상관 x )
 							else {
 								//play[idx][i] = now;
 								copy[idx][i] = now;
 								temp = now;
-								flag = true;
 								idx++;
 							}
+					
 						}
 					}
 					break;	
 			}
+			
+			System.out.println("change~! depth: "+depth);
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					System.out.printf("%d ",copy[i][j]);
+				}
+				System.out.println();
+			}	
+			
+			System.out.println("--------------------");
+			System.out.println();
 			
 			
 //			for(int n = 0; n < N; n++) {
@@ -237,6 +274,7 @@ public class Easy2048 {
 //					}
 //				}
 //			}	
+			
 			start(depth+1,copy);
 		}
 		
