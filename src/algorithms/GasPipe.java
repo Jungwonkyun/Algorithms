3e00983e81d228de79695078f864fec64e96a820
정원균 <wjddnjsrbs97@naver.com>
2023-02-26 21:51:48 +0900

    2023.02.26

--- src/algorithms/GasPipe.java
+++ src/algorithms/GasPipe.java
@@ -10,10 +10,8 @@ public class GasPipe {
 	static int R;
 	static int C;
 	static char[][] map;
-	static int[] dx = {-1,1,0,0};
+	static int[] dx = {-1,1,0,0};  
 	static int[] dy = {0,0,-1,1};
-	static int pbx = -1;
-	static int pby = -1;
 	static int bx = -1;
 	static int by = -1;
 	static boolean[][] visited;
@@ -29,15 +27,12 @@ public class GasPipe {
 		C = Integer.parseInt(input[1]);
 		map = new char[R][C];
 		visited = new boolean [R][C];
+		char result = '0';
 		
-		int Zx = 0;
-		int Zy = 0;
 		int Mx = 0;
 		int My = 0;
 		
-		
-		
-		
+	
 		for(int i = 0; i < R; i++) {
 			String in = br.readLine();
 			for(int j = 0; j < C; j++) {
@@ -46,124 +41,69 @@ public class GasPipe {
 					Mx = i;
 					My = j;
 				}
-				else if(in.charAt(j)=='Z') {
-					Zx = i;
-					Zy = j;
-				}
 			}
 		}
 
-		for(int i = 0; i < R; i++) {
-			for(int j = 0; j < C; j++) {
-				System.out.print(map[i][j]+" ");
-			}System.out.println();
-		}
 		int nMx = 0;
 		int nMy = 0;
-		int nZx = 0;
-		int nZy = 0;
 		
 		for(int i = 0; i<4; i++) {
 			nMx = Mx + dx[i];
 			nMy = My + dy[i];
-			
-			if(nMx<0||nMx>=R||nMy<0||nMy>=C||map[nMx][nMy] == '.')continue;
+			if(nMx<0||nMx>=R||nMy<0||nMy>=C||map[nMx][nMy] == '.'||map[nMx][nMy] == 'M'||map[nMx][nMy] == 'Z')continue;
 			visited[Mx][My] = true;
 			findBreak(Mx,My,nMx,nMy);
 			break;
 		}
 		
-		//System.out.println(bx+" "+by);
 		
 		int tx;
 		int ty;
+		int[] cnt = new int[4];
+		int c = 0;
+		
 		for(int i = 0; i<4; i++) {
 			tx = bx+dx[i];
 			ty = by+dy[i];
 			
-			if(map[tx][ty] != '.' && tx != pbx && ty !=pby){
-				//System.out.println("good "+ bx+" "+by+" "+tx+" "+ty);
-				//from up
-				if(bx-tx ==1) {
-					dir2 = 1;
-				}
-				//from bottom
-				else if(bx-tx ==-1) {
-					dir2 = 2;
-				}
-				//from left
-				else if(by-ty == 1) {
-					dir2 = 3;
-				}
-				//from right
-				else{
-					dir2 = 4;
-				}
-		
+			if(tx<0||tx>=R||ty<0||ty>=C||map[tx][ty]=='M'||map[tx][ty]=='Z'||map[tx][ty]=='.')continue;
+			
+			//안되는 케이스는 빼줘야한다 
+			if(i==0) {
+				if(map[tx][ty] == '2' ||map[tx][ty] == '3'||map[tx][ty] == '-')continue;
 			}
+			else if(i==1) {
+				if(map[tx][ty] == '1' ||map[tx][ty] == '4'||map[tx][ty] == '-')continue;
+			}
+			else if(i==2) {
+				if(map[tx][ty] == '3' ||map[tx][ty] == '4'||map[tx][ty] == '|')continue;
+			}
+			else if(i==3){
+				if(map[tx][ty] == '1' ||map[tx][ty] == '2'||map[tx][ty] == '|')continue;
+			}
+			
+			cnt[i]++;
+			c++;
 			
 		}
 		
-		System.out.println(dir1+" "+dir2);
-		char result = '0';
-		//from up
-		if(dir1 == 1) {
-			if(dir2 == 2) {
-				result = '|';
-			}
-			else if(dir2 == 3) {
-				result = '3';
-			}
-			else {
-				result = '2';
-			}
-			
-		}
-		//from bottom
-		else if(dir1 == 2) {
-			if(dir2 == 1) {
-				result = '|';
-			}
-			else if(dir2 == 3) {
-				result = '4';
-			}
-			else {
-				result = '1';
-			}
+		if(c == 4) {
+			result = '+';
+		}else {
+			if(cnt[0] == 1 && cnt[1] == 1)result = '|';
+			else if(cnt[0] == 1 && cnt[2] == 1)result = '3';
+			else if(cnt[0] == 1 && cnt[3] == 1)result = '2';
+			else if(cnt[1] == 1 && cnt[2] == 1)result = '4';
+			else if(cnt[1] == 1 && cnt[3] == 1)result = '1';
+			else if(cnt[2] == 1 && cnt[3] == 1)result = '-';
 		}
 		
-		//from left
-		else if(dir1 == 3) {
-			if(dir2 == 1) {
-				result = '3';
-			}
-			else if(dir2 == 2) {
-				result = '4';
-			}
-			else {
-				result = '-';
-			}	
-		}
-		else {
-			if(dir2 == 1) {
-				result = '2';	
-			}
-			else if(dir2 == 2) {
-				result = '1';
-				
-			}
-			else if(dir2 == 3) {
-				result = '-';
-			}
-			
-		}
 		
-		System.out.println((bx+1)+" "+(by+1)+" "+result);
+		System.out.println((bx+1)+" "+(by+1)+" "+Character.toString(result));
 
 	}
 
 	public static boolean findBreak(int prevx, int prevy, int nowx, int nowy) {
-		//System.out.println(prevx+" "+prevy+" "+nowx+" "+nowy);
 		//다음 칸이 방문한적이 있다면 가스관이 연결 되었다는 의미 
 		if(visited[nowx][nowy] == true)return true;
 		
@@ -175,25 +115,6 @@ public class GasPipe {
 			if(bx == -1 && by == -1) {
 				bx = nowx;
 				by = nowy;
-				pbx = prevx;
-				pby = prevy;
-				
-				//from up
-				if(bx-pbx ==1) {
-					dir1 = 1;
-				}
-				//from bottom
-				else if(bx-pbx ==-1) {
-					dir1 = 2;
-				}
-				//from left
-				else if(by-pby ==1) {
-					dir1 = 3;
-				}
-				//from right
-				else{
-					dir1 = 4;
-				}
 				return false;
 			}
 		}
@@ -218,6 +139,8 @@ public class GasPipe {
 			//from bottom to right
 			else {
 				flag = findBreak(nowx,nowy,nowx,nowy+1);
+				
+				
 			}
 			break;
 			
---
+++ src/gumi_five/CupRotate.java
@@ -0,0 +1,19 @@
+package gumi_five;
+
+import java.util.Scanner;
+
+public class CupRotate {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		
+		for(int i = 1; i < tc+1; i++) {
+			int N = sc.nextInt();
+			int X = sc.nextInt();
+			int K = sc.nextInt();
+			
+		}
+	}
+
+}
