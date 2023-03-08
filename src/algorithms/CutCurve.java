800a013e0554cd154eb0606ff392a4e2580f0201
정원균 <wjddnjsrbs97@naver.com>
2023-03-08 23:42:01 +0900

    2023.03.08

---
+++ src/algorithms/CutCurve.java
@@ -0,0 +1,29 @@
+package algorithms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class CutCurve {
+	
+	static int N;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		ArrayList<Integer>left = new ArrayList<>();
+		ArrayList<Integer>right = new ArrayList<>();
+		
+		int firstX;
+		int firstY;
+		
+		
+		for(int i = )
+		
+		
+		
+	}
+
+}
